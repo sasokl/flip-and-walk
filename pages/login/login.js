36105e2637954c9f8ebc1dcbd5597de3d9becf8a
Alexander Blaus <devsasokl@gmail.com>
2021-10-07 22:05:09 +0300

    login page finished

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,24 +3,44 @@ const rootPath = window.location.protocol + "//" + window.location.host;
 const loginPage = document.querySelector('.login-page__resolution-wrapper');
 
 const loginTemplate = document.querySelector('#login-template').content;
-const signInNode = loginTemplate.querySelector('.signin').cloneNode(true);
+const signNode = loginTemplate.querySelector('.sign').cloneNode(true);
+const recoveryNode = loginTemplate.querySelector('.recovery').cloneNode(true);
+
+const signInNode = loginTemplate.querySelector('#signin').cloneNode(true);
 const signInForm = signInNode.querySelector('#signin-form');
-const signupLink = signInNode.querySelector('#signup-link');
+const signupLink = signInNode.querySelector('#signup-link'); // the link that opens signUp
 const forgotLink = signInNode.querySelector('.form__forgot-text');
 
-const signUpNode = loginTemplate.querySelector('.signup').cloneNode(true);
-//const signUpButton = signUpNode.querySelector('#signup-button');
+const signUpNode = loginTemplate.querySelector('#signup').cloneNode(true);
+const signUpForm = signUpNode.querySelector('#signup-form');
+const signinLink = signUpNode.querySelector('#signin-link'); // the link that opens signIn
 
-loginPage.append(signInNode);
+const backButton = recoveryNode.querySelector('#back-button');
 
 
+//loginPage.append(signNode);
+loginPage.prepend(recoveryNode);
+signNode.appendChild(signInNode);
 
 signInForm.addEventListener('submit', (e) => {
     e.preventDefault();
     window.location.href = rootPath + "/flip-and-walk/pages/flips/flips.html";
 });
+
 signupLink.addEventListener('click', () => {
-    loginPage.replaceChild(signUpNode, signInNode);
+    signNode.replaceChild(signUpNode, signInNode);
+});
+
+signinLink.addEventListener('click', () => {
+    signNode.replaceChild(signInNode, signUpNode);
+});
+
+forgotLink.addEventListener('click', () => {
+    loginPage.replaceChild(recoveryNode,signNode);
+});
+
+backButton.addEventListener('click',() => {
+    loginPage.replaceChild(signNode,recoveryNode);
 });
 
 
