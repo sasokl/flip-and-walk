fbf1970a737747b4efc6072b9afe959cb275450b
Alexander Blaus <devsasokl@gmail.com>
2021-10-07 18:53:51 +0300

    signin finished

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,20 +1,26 @@
 
+const rootPath = window.location.protocol + "//" + window.location.host;
 const loginPage = document.querySelector('.login-page__resolution-wrapper');
 
 const loginTemplate = document.querySelector('#login-template').content;
 const signInNode = loginTemplate.querySelector('.signin').cloneNode(true);
-const signInButton = signInNode.querySelector('#signin-button');
+const signInForm = signInNode.querySelector('#signin-form');
+const signupLink = signInNode.querySelector('#signup-link');
+const forgotLink = signInNode.querySelector('.form__forgot-text');
 
 const signUpNode = loginTemplate.querySelector('.signup').cloneNode(true);
-const signUpButton = signUpNode.querySelector('#signup-button');
+//const signUpButton = signUpNode.querySelector('#signup-button');
 
 loginPage.append(signInNode);
 
-signInButton.addEventListener('click', () => {
-    loginPage.replaceChild(signUpNode,signInNode);
+
+
+signInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location.href = rootPath + "/flip-and-walk/pages/flips/flips.html";
+});
+signupLink.addEventListener('click', () => {
+    loginPage.replaceChild(signUpNode, signInNode);
 });
 
-signUpButton.addEventListener('click', () => {
-    loginPage.replaceChild(signInNode,signUpNode);
-});
 
